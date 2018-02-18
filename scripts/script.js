97e79c215234860b3223c801ca63fca7ffaf9806
Brian Juul Andersen <mail@kree8tive.dk>
2018-02-18 12:07:49 +0100

    Add glyphicons. Refactor markup and styles

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,57 @@
-$('.contactInfo').click(function(){ window.open('mailto:mail@kree8tive.dk?subject=Hi there ...&body=Hello%0A%0AI want to talk to you about...' ,'_blank'); });
+/--- contact --/
+function addListener(element, eventName, handler) {
+	if (element.addEventListener) {
+		element.addEventListener(eventName, handler, false);
+	}
+	else if (element.attachEvent) {
+		element.attachEvent('on' + eventName, handler);
+	}
+	else {
+		element['on' + eventName] = handler;
+	}
+}
+addListener(
+	document.getElementById('contactInfo'),
+	'click', function(){
+	window.open('mailto:mail@kree8tive.dk?subject=Hi there ...&body=Hello%0A%0AI want to talk to you about...' ,'_blank');
+});
+
+/--- Background ---/
+var imgs = [
+	"./img/plasma01.png",
+	"./img/plasma02.png",
+	"./img/plasma03.png",
+	"./img/plasma04.png",
+	"./img/plasma05.png",
+	"./img/plasma06.png",
+	"./img/plasma07.png"];
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var counter = 0;
+var backgroundImage = new Image();
+
+backgroundImage.onload = function(){
+	ctx.drawImage(
+		backgroundImage, 0, 0, backgroundImage.width, backgroundImage.height,
+		0, 0, canvas.width, canvas.height * 2
+	);
+}
+backgroundImage.src = imgs[0];
+
+setInterval(function(){
+	nextImage();
+}, 10000);
+
+function nextImage(){
+	counter++;
+	if(counter < imgs.length - 1){
+		backgroundImage.src = imgs[counter];
+	}else{
+		counter = 0;
+	}
+};
