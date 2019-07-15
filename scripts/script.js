133989ce7e4be7327b1067843f45f9d75c3c0667
Brianmanden <brianmanden@gmail.com>
2019-07-15 22:53:25 +0200

    WIP - changed to using Bootstrap Cards and Jumbotron

--- scripts/script.js
+++ scripts/script.js
@@ -17,39 +17,6 @@ addListener(
 	window.open('mailto:mail@kree8tive.dk?subject=Hi there ...&body=Hello%0A%0AI want to talk to you about...' ,'_blank');
 });
 
-/--- Background ---/
-var imgs = [
-	"./img/plasma01.png",
-	"./img/plasma02.png",
-	"./img/plasma03.png",
-	"./img/plasma04.png",
-	"./img/plasma05.png",
-	"./img/plasma06.png",
-	"./img/plasma07.png"
-];
-
-var canvas = document.getElementById("my-canvas");
-var ctx = canvas.getContext("2d");
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-var counter = 0;
-var backgroundImage = new Image();
-
-backgroundImage.onload = function(){
-	ctx.drawImage(
-		backgroundImage, 0, 0, backgroundImage.width, backgroundImage.height,
-		0, 0, canvas.width, canvas.height * 2
-	);
-}
-backgroundImage.src = imgs[0];
-
-setInterval(function(){
-	nextImage();
-}, 60000);
-
-function nextImage(){
-	counter++;
-	counter < imgs.length - 1 ? backgroundImage.src = imgs[counter] : counter = 0;
-};
+$(document).ready(function() {
+	$('.carousel').carousel({interval: 6500});
+  });
