e6ec2d85bb39378e19cdcedf78bc9210981066da
Brianmanden <brianmanden@gmail.com>
2020-05-13 21:59:20 +0200

    Remove unused files and testing webhook

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,24 @@
-/--- contact --/
+/--- contact --/;
 function addListener(element, eventName, handler) {
-	if (element.addEventListener) {
-		element.addEventListener(eventName, handler, false);
-	}
-	else if (element.attachEvent) {
-		element.attachEvent('on' + eventName, handler);
-	}
-	else {
-		element['on' + eventName] = handler;
-	}
+  if (element.addEventListener) {
+    element.addEventListener(eventName, handler, false);
+  } else if (element.attachEvent) {
+    element.attachEvent("on" + eventName, handler);
+  } else {
+    element["on" + eventName] = handler;
+  }
 }
 
-addListener(
-	document.getElementById('contact-info'),
-	'click', function(){
-		window.open('mailto:mail@kree8tive.dk?subject=Hi there ...&body=Hello%0A%0AI want to talk to you about...' ,'_blank');
-	}
-);
+addListener(document.getElementById("contact-info"), "click", function() {
+  window.open(
+    "mailto:mail@kree8tive.dk?subject=Hi there ...&body=Hello%0A%0AI want to talk to you about...",
+    "_blank"
+  );
+});
 
 $(document).ready(function() {
-	$('.carousel').carousel({
-		interval: 5500,
-		pause: 'none'
-	})
+  $(".carousel").carousel({
+    interval: 5500,
+    pause: "none"
   });
+});
